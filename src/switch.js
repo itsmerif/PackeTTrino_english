0a70fd05ae1fd45da92978b70bb718090aa7291a
EvilPrime98 <joseamin1998@gmail.com>
2025-02-04 15:01:46 +0100

    mac table

--- src/switch.js
+++ src/switch.js
@@ -3,6 +3,7 @@ function createSwitchObject(x, y) {
     const board = document.querySelector(".board");
     const networkObject = document.createElement("article");
     const networkObjectIcon = document.createElement("img");
+    const networkObjectTable = document.createElement("article");
 
     networkObject.id = `switch-${itemIndex}`;
 
@@ -11,9 +12,21 @@ function createSwitchObject(x, y) {
     networkObjectIcon.draggable = true;
     networkObject.appendChild(networkObjectIcon);
 
+    networkObjectTable.classList.add("mac-table");
+    networkObjectTable.innerHTML = `
+            <table>
+                <tr>
+                    <th>Port</th>
+                    <th>MAC Address</th>
+                </tr>
+            </table>
+            <button onclick="closeMacTable(event)">Cerrar</button>`;
+
+    networkObject.appendChild(networkObjectTable);
 
     networkObject.addEventListener("dragstart", event => BoardItemDragStart(event));
     networkObject.addEventListener("drop", switchConn);
+    networkObject.addEventListener("click", showMacTable);
 
 
     networkObject.classList.add("item-dropped", "switch");
@@ -31,17 +44,46 @@ function switchConn(event) {
     event.stopPropagation();
 
     const item = event.dataTransfer.getData("json");
+    const networkObject = event.target.closest(".item-dropped");
 
     if (item) {
-        
-        const itemType = JSON.parse(item).itemType;
-        const itemId = JSON.parse(item).itemId;
-        const x1 = JSON.parse(item).originx;
-        const y1 = JSON.parse(item).originy;
+
+        const itemType = JSON.parse(item).itemType; //averiguo si es un item dropeable
+        const itemId = JSON.parse(item).itemId; //obtengo el id del item
+        const x1 = JSON.parse(item).originx; //obtengo el x del elemento origen
+        const y1 = JSON.parse(item).originy; //obtengo el y del elemento origen
+        const mac = JSON.parse(item).mac;
 
         if (itemType === "item-dropped" && (itemId.startsWith("pc-") || itemId.startsWith("router-") || itemId.startsWith("server-"))) {
-            createCableObject(x1, y1, event.clientX, event.clientY);
+            createCableObject(x1, y1, networkObject.style.left, networkObject.style.top);
+            saveMac(event, mac);
         }
 
     }
+}
+
+function showMacTable(event) {
+    event.stopPropagation();
+    const networkObject = event.target.closest(".item-dropped");
+    const table = networkObject.querySelector(".mac-table");
+    table.style.display = "flex";
+}
+
+function closeMacTable(event) {
+    event.stopPropagation();
+    const networkObject = event.target.closest(".item-dropped");
+    const table = networkObject.querySelector(".mac-table");
+    table.style.display = "none";
+}
+
+function saveMac(event, mac) {
+    const networkObject = event.target.closest(".item-dropped");
+    const networtTable = networkObject.querySelector("table");
+    const newMac = document.createElement("tr");
+    newMac.innerHTML = `
+        <tr>
+            <td>1</td>
+            <td>${mac}</td>
+        </tr>`;
+    networtTable.appendChild(newMac);
 }
