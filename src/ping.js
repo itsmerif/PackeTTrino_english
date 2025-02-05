bab1f84e5ae72f7e061eccfdd7f423f974c52337
EvilPrime98 <joseamin1998@gmail.com>
2025-02-05 22:17:34 +0100

    no comments ping

--- src/ping.js
+++ src/ping.js
@@ -6,37 +6,35 @@ function ping( originIP, destinationIP ) {
 
     const origin = document.querySelector(`[data-ip="${originIP}"]`);
     const originId = origin.id;
-    
-    //ahora toca mirar la tabla ARP del equipo origen
-
     const arpTable = getARPTable(originId);
 
     for (let i = 0; i < arpTable.length; i++) {
+
         const arpRow = arpTable[i];
         const mac = arpRow[1];
         const ip = arpRow[0];
+
         if (ip === destinationIP) {
             return `La IP ${ip} está en el MAC ${mac}`;
         }
     }
 
-    //
-    const NetworkOriginObject = document.getElementById(originId); //obtengo el elemento con ese id
-    const switchIdentity = NetworkOriginObject.getAttribute("data-switch");    //de ese origen obtengo el switch al que está conectado
+    const NetworkOriginObject = document.getElementById(originId);
+    const switchIdentity = NetworkOriginObject.getAttribute("data-switch");
     const switchOriginObject = document.getElementById(switchIdentity);
 
-    const macElements = switchOriginObject.querySelector("table").querySelectorAll(".mac-address");   //de ese switch obtengo la tabla de macs   
+    const macElements = switchOriginObject.querySelector("table").querySelectorAll(".mac-address"); 
     let macs = [];
     for (let i = 0; i < macElements.length; i++) {
         macs.push(macElements[i].innerHTML);
     }
 
-    for (let i = 0; i < macs.length; i++) {  //por cada mac obtengo el puntero del elemento, y del elemento obtengo la ip
+    for (let i = 0; i < macs.length; i++) {
         const mac = macs[i];
         const pc = document.querySelector(`[data-mac="${mac}"]`);
         const ip = pc.getAttribute("data-ip");
         
-        if (destinationIP === ip) { //compruebo si la ip es igual a la de destino
+        if (destinationIP === ip) {
             ping_s(originIP);
             return `Haciendo ping a ${destinationIP} con 32 bytes de datos:\n\n`;
         }
