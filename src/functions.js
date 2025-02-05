c28d9b6876d51d57a1c5e0d3a46569434bab0690
EvilPrime98 <joseamin1998@gmail.com>
2025-02-05 13:10:15 +0100

    ping changes

--- src/functions.js
+++ src/functions.js
@@ -41,54 +41,43 @@ function ipconfig(id) {
 
 function ping( originIP, destinationIP ) {
 
-    let response =  "";
+    if (!originIP) {
+        return "IP no configurada.";
+    }
 
     //obtengo el origen y compruebo que exista y no esté duplicado
     const origin = document.querySelector(`[data-ip="${originIP}"]`);
     const allOrigins = document.querySelectorAll(`[data-ip="${originIP}"]`);
     
-    if (!origin) {
-        return "IP no configurada."; //early return para salirnos del codigo
-    }
-    
-    if (allOrigins.length > 1) {
-        return "Error: Se encontró más de un elemento con la misma IP"; //early return para salirnos del codigo
+    if (allOrigins.length > 1) { //si hay más de un origen con esa ip
+        return "Error: Se encontró más de un elemento con la misma IP";
     }
 
-    //obtengo el id del origen
-    const originId = origin.id;
+    const originId = origin.id;  //obtengo el id del origen
     const NetworkOriginObject = document.getElementById(originId);
 
-    //de ese origen obtengo el switch al que está conectado
-    const switchIdentity = NetworkOriginObject.getAttribute("data-switch");
+    const switchIdentity = NetworkOriginObject.getAttribute("data-switch");    //de ese origen obtengo el switch al que está conectado
     const switchOriginObject = document.getElementById(switchIdentity);
 
-    //de ese switch obtengo la tabla de macs
-    const macElements = switchOriginObject.querySelector("table").querySelectorAll(".mac-address");      
+    const macElements = switchOriginObject.querySelector("table").querySelectorAll(".mac-address");   //de ese switch obtengo la tabla de macs   
     let macs = [];
     for (let i = 0; i < macElements.length; i++) {
         macs.push(macElements[i].innerHTML);
     }
 
-    //por cada mac obtengo el puntero del elemento, y del elemento obtengo la ip
-    for (let i = 0; i < macs.length; i++) {
+    for (let i = 0; i < macs.length; i++) {  //por cada mac obtengo el puntero del elemento, y del elemento obtengo la ip
         const mac = macs[i];
         const pc = document.querySelector(`[data-mac="${mac}"]`);
         const ip = pc.getAttribute("data-ip");
-        //compruebo si la ip es igual a la de destino
-        if (destinationIP === ip) {
+        
+        if (destinationIP === ip) { //compruebo si la ip es igual a la de destino
             ping_s(originIP);
             return `Haciendo ping a ${destinationIP} con 32 bytes de datos:\n\n`;
         }
     }
     
-    //si no encuentro nada devuelvo un mensaje de error
-    if (response === "") {
-        response = `From ${originIP} icmp_seq=1 Destination Host Unreachable`;
-    }
-
-    //finalmente devolvemos el mensaje
-    return response;
+    ping_f(originIP);
+    return `Haciendo ping a ${destinationIP} con 32 bytes de datos:\n\n`;
     
 }
 
@@ -97,6 +86,15 @@ function ping_s(origin) {
     const terminalOutput = document.querySelector(".terminal-output");
     window.pingInterval = setInterval(() => {
         terminalOutput.innerHTML +=`64 bytes from ${origin}: icmp_seq=1 ttl=64 time=0.030 ms\n`;
+    }, 1000);
+
+}
+
+function ping_f(origin) {
+
+    const terminalOutput = document.querySelector(".terminal-output");
+    window.pingInterval = setInterval(() => {
+        terminalOutput.innerHTML +=`From ${origin} icmp_seq=1 Destination Host Unreachable\n`;
     }, 1000);
 
 }
