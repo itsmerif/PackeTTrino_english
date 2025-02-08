7d266afa27e267c9a652236bdb66e928c368bf98
EvilPrime98 <joseamin1998@gmail.com>
2025-02-08 17:21:31 +0100

    ping fix

--- src/ping.js
+++ src/ping.js
@@ -65,6 +65,7 @@ async function ping(originIP, destinationIP, visual = false) {
         }
 
         //el equipo destino está en la tabla ARP del equipo origen
+
         const destinationMac = isIpInARPTable(originId, destinationIP); // Hemos encontrado la mac del equipo destino. Enviamos una trama unicast al switch con esa mac de destino
 
         if (visual) {
@@ -73,7 +74,7 @@ async function ping(originIP, destinationIP, visual = false) {
         }
 
         saveMac(switchOriginObjectId, originId, originObjectMac); //si el origen no es conocido para el switch, añadimos la mac al switch
-
+ 
         if (!isMacInMACTable(switchOriginObjectId, destinationMac)) { //la mac de destino NO existe en la tabla del switch
 
             //ahora el switch satura todos los puertos e intenta encontrar la mac de destino
@@ -82,7 +83,7 @@ async function ping(originIP, destinationIP, visual = false) {
                 broadcastSwitch(switchOriginObjectId, originId);
                 await waitForMove();
             }
-            
+
             if (!isMacinNetwork(switchOriginObjectId, destinationMac)) { //ninguno de los equipos acepta la trama y se da por fallido
                 if (!visual) ping_f(originIP);
                 return;
@@ -96,9 +97,17 @@ async function ping(originIP, destinationIP, visual = false) {
             return;
         }
 
-        //la mac de destino existe en la tabla del switch. el switch envia una trama unicast al destino.         
-        //una vez que llega al equipo, debemos hacer comprobaciones a nivel de enlace de datos y de red
+        //la mac de destino existe en la tabla del switch. el switch envia una trama unicast al destino.
+
         const networkDestinationObjectId = getDeviceFromMac(switchOriginObjectId, destinationMac);
+        const networkDestinationObject = document.getElementById(networkDestinationObjectId);
+
+        if (visual) {
+            movePacket(switchOriginObject.style.left, switchOriginObject.style.top, networkDestinationObject.style.left, networkDestinationObject.style.top, "unicast");
+            await waitForMove();
+        }
+
+        //una vez que llega al equipo, debemos hacer comprobaciones a nivel de enlace de datos y de red
 
         if (!macCheck(networkDestinationObjectId, destinationMac)) { //si la mac de destino en la trama no coincide con la mac del equipo, se da por fallido
             if (!visual) ping_f(originIP);
@@ -110,7 +119,15 @@ async function ping(originIP, destinationIP, visual = false) {
             return;
         }
 
-        if (!visual) ping_s(originIP); //si todo sale bien, se da por exitoso
+        if (visual) {
+            movePacket(networkDestinationObject.style.left, networkDestinationObject.style.top, switchOriginObject.style.left, switchOriginObject.style.top, "unicast");
+            await waitForMove();
+            movePacket(switchOriginObject.style.left, switchOriginObject.style.top, NetworkOriginObject.style.left, NetworkOriginObject.style.top, "unicast");
+        }else {
+            ping_s(originIP); //si todo sale bien, se da por exitoso
+            return;
+        }
+
     }
 }
 
