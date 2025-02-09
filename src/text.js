b217da6139c51eb48aaf50f4a53567fed030d809
EvilPrime98 <joseamin1998@gmail.com>
2025-02-09 07:30:58 +0100

    annotation autoextend

--- src/text.js
+++ src/text.js
@@ -2,17 +2,39 @@ function createTextObject(x, y) {
 
     const board = document.querySelector(".board");
     const textObject = document.createElement("article");
+    const input = document.createElement("input");
 
     textObject.id = "text-" + itemIndex;
     textObject.classList.add("text-annotation");
     textObject.style.left = `${x}px`;
     textObject.style.top = `${y}px`;
-    textObject.innerHTML = `<input type="text">`;
     textObject.draggable = true;
     textObject.addEventListener("dragstart", event => BoardItemDragStartText(event));
+
+    input.type = "text";
+    input.addEventListener("input", autoExtendText);
+    textObject.appendChild(input);
+
     board.appendChild(textObject);
     itemIndex++;
 
+}
+
+function autoExtendText() {
+    const container = this.parentElement;
+    const input = container.querySelector("input");
+    const temp = document.createElement('span');
+    temp.style.visibility = 'hidden';
+    temp.style.position = 'absolute';
+    temp.style.whiteSpace = 'pre';
+    temp.style.font = window.getComputedStyle(input).font;
+    temp.textContent = this.value || 'W';  
+    document.body.appendChild(temp);
+    const width = temp.getBoundingClientRect().width;
+    document.body.removeChild(temp); 
+    const newWidth = Math.max(40, width + 20);
+    container.style.width = `${newWidth}px`;
+    container.style.marginLeft = `-${newWidth/2}px`;
 }
 
 function BoardItemDragStartText(event) {
