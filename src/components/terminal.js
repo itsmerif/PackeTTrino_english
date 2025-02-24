1d1214a5b070b4503e79abe24afbb20bd5768cee
EvilPrime98 <joseamin1998@gmail.com>
2025-02-24 07:22:46 +0100

    file editor improve css

--- src/components/terminal.js
+++ src/components/terminal.js
@@ -129,6 +129,7 @@ function command_help() {
     terminalMessage("<li>firewall: utilidad para configurar cortafuegos en dispositivos</li>");
     terminalMessage("<li>ip: utilidad para mostrar/administrar la configuracion de red o enrutamiento en dispositivos</li>");
     terminalMessage("<li>dig: utilidad para resolver nombres de dominio</li>");
+    terminalMessage("<li>nano: utilidad para editar archivos</li>");
     terminalMessage("<li>help: mostrar ayuda del terminal</li>");
     terminalMessage("<li>exit: salir del terminal</li>");
 }
