d43c0c286f81acdb1c22a666f3f78c9bf96ef86c
EvilPrime98 <joseamin1998@gmail.com>
2025-02-26 00:34:47 +0100

    added visual element

--- src/protocols/visualizer.js
+++ src/protocols/visualizer.js
@@ -1,3 +1,15 @@
-function visualize(originObject, destinationObject, packet) {
-    console.log(originObject, destinationObject, packet.protocol);
+async function visualize(originObject, destinationObject, packet) {
+    const $originObject = document.getElementById(originObject);
+    const $destinationObject = document.getElementById(destinationObject);
+    
+    if (packet.destination_mac === "ff:ff:ff:ff:ff:ff") {
+
+        type = "broadcast";
+
+    } else {
+        type = "unicast";
+        
+    }
+
+    await movePacket($originObject.style.left, $originObject.style.top, $destinationObject.style.left, $destinationObject.style.top, type);
 }
