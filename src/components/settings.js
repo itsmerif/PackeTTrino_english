e31c4ae135911db4cfa82540f5ba292fb6f59ee4
EvilPrime98 <joseamin1998@gmail.com>
2025-03-12 22:37:13 +0100

    added a settings panel item

--- src/components/settings.js
+++ src/components/settings.js
@@ -4,11 +4,25 @@ function showOptions() {
     modalComponent.classList.add("settings-modal-container");
     modalComponent.innerHTML = `
     <div class="modal-overlay"></div>
-    <div class="settings-modal">
-        <p>Opciones</p>
-        <button class="btn-close" onclick="closeSettingsModal()">Cerrar</button>
-    </div>`;
+    <form class="settings-modal">
+        <div class="form-group">
+            <label for="ignore-arp-traffic"> Ignorar tráfico ARP (Visual) </label>
+            <input type="checkbox" id="ignore-arp-traffic" name="ignore-arp-traffic">
+        </div>
+        <button class="btn-blue">Aceptar</button>
+    </form>`;
 
+    if (ignoreArpTraffic) {
+        modalComponent.querySelector("#ignore-arp-traffic").checked = true;
+    }
+
+    modalComponent.querySelector("#ignore-arp-traffic").addEventListener("change", function () {ignoreArpTraffic = this.checked;});
+    modalComponent.querySelector(".btn-blue").addEventListener("click", closeSettingsModal);
     document.body.appendChild(modalComponent);
 
+}
+
+function closeSettingsModal() {
+    const modalComponent = document.querySelector(".settings-modal-container");
+    modalComponent.remove();
 }
