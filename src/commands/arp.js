bad17cd7e3fd03d2412773eaeb7d5fde1d540a28
EvilPrime98 <joseamin1998@gmail.com>
2025-03-16 13:04:48 +0100

    added arp flush

--- src/commands/arp.js
+++ src/commands/arp.js
@@ -2,8 +2,18 @@ function command_arp(dataId, args) {
 
     const $networkObject = document.getElementById(dataId);
 
-    if (args[1] === "-n") {
+    if (args[1] === "-s" || args[1] === "--show") {
         terminalMessage($networkObject.querySelector(".arp-table").querySelector("table").outerHTML);
+        return;
+    }
+
+    if (args[1] === "-f" || args[1] === "--flush") {
+        $networkObject.querySelector(".arp-table").querySelector("table").innerHTML = `
+                <tr>
+                    <th>IP Address</th>
+                    <th>MAC Address</th>
+                </tr>`;
+        terminalMessage("La tabla ARP ha sido limpiada correctamente.");
         return;
     }
 
