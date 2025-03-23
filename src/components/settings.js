d5b20a0c97e84e6cfc27d79270cd37148987d614
EvilPrime98 <joseamin1998@gmail.com>
2025-03-23 15:27:48 +0100

    added visual mode to settings

--- src/components/settings.js
+++ src/components/settings.js
@@ -1,23 +1,33 @@
 function showOptions() {
 
     const modalComponent = document.createElement("div");
+    const $btnStyle = "btn-green";
     modalComponent.classList.add("settings-modal-container");
     modalComponent.innerHTML = `
     <div class="modal-overlay"></div>
     <form class="settings-modal">
         <div class="form-group">
+            <label for="visual-toggle"> Visualización (Visual) </label>
+            <input type="checkbox" id="visual-toggle" name="visual-toggle">
+        </div>
+        <div class="form-group">
             <label for="ignore-arp-traffic"> Ignorar tráfico ARP (Visual) </label>
             <input type="checkbox" id="ignore-arp-traffic" name="ignore-arp-traffic">
         </div>
-        <button class="btn-blue">Aceptar</button>
+        <button class=${$btnStyle}>Aceptar</button>
     </form>`;
 
     if (ignoreArpTraffic) {
         modalComponent.querySelector("#ignore-arp-traffic").checked = true;
     }
 
+    if (visualToggle) {
+        modalComponent.querySelector("#visual-toggle").checked = true;
+    }
+
     modalComponent.querySelector("#ignore-arp-traffic").addEventListener("change", function () {ignoreArpTraffic = this.checked;});
-    modalComponent.querySelector(".btn-blue").addEventListener("click", closeSettingsModal);
+    modalComponent.querySelector("#visual-toggle").addEventListener("change", function () {visualToggle = this.checked;});
+    modalComponent.querySelector(`.${$btnStyle}`).addEventListener("click", closeSettingsModal);
     document.body.appendChild(modalComponent);
 
 }
