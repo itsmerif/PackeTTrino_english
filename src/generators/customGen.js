292a4c7d43a35670b3f4834331f57b0f92ddfa49
EvilPrime98 <joseamin1998@gmail.com>
2025-03-30 22:42:35 +0200

    refactor with arpResolver in customGen

--- src/generators/customGen.js
+++ src/generators/customGen.js
@@ -5,46 +5,39 @@ async function customPacketGenerator(networkObjectId, packet) {
     const switchId = $networkObject.getAttribute("data-switch");
     const destination_ip = packet.destination_ip;
     const isSameNetwork = getNetwork(networkObjectIp, $networkObject.getAttribute("data-netmask")) === getNetwork(destination_ip, $networkObject.getAttribute("data-netmask"));
-    let destination_mac;
 
     if (!isSameNetwork) {
 
         const defaultGateway = $networkObject.getAttribute("data-gateway");
 
-        if (!defaultGateway) {
-            throw new Error("Error: Puerta de Enlace Predeterminada No Configurada");
-        }
+        if (!defaultGateway) throw new Error("Error: Puerta de Enlace Predeterminada No Configurada");
 
         const defaultGatewayMac = isIpInARPTable(networkObjectId, defaultGateway);
 
         if (!defaultGatewayMac) {
             buffer[networkObjectId] = packet;
-            let newPacket = new ArpRequest(networkObjectIp, defaultGateway, $networkObject.getAttribute("data-mac"));
-            addPacketTraffic(newPacket);
-            await switchProcessor(switchId, networkObjectId, newPacket);
+            arpResolver(networkObjectId, defaultGateway);
             return;
         }
 
         packet.destination_mac = defaultGatewayMac;
-        icmpFlag = false;
         addPacketTraffic(packet);
         await switchProcessor(switchId, networkObjectId, packet);
         return;
 
     }
 
-    //están en la misma red
 
-    packet.destination_mac = isIpInARPTable(networkObjectId, destination_ip);
+    const destination_mac = isIpInARPTable(networkObjectId, destination_ip);
 
-    if (!packet.destination_mac) {
+    if (!destination_mac) {
         buffer[networkObjectId] = packet;
-        let newPacket = new ArpRequest(networkObjectIp, destination_ip, $networkObject.getAttribute("data-mac"));
-        addPacketTraffic(newPacket);
-        await switchProcessor(switchId, networkObjectId, newPacket);
-    } else {
-        addPacketTraffic(packet);
-        await switchProcessor(switchId, networkObjectId, packet);
+        arpResolver(networkObjectId, destination_ip);
+        return;
     }
+
+    packet.destination_mac = destination_mac;
+    addPacketTraffic(packet);
+    await switchProcessor(switchId, networkObjectId, packet);
 
 }
