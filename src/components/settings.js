75dd9cd04c961da39a1e01aeeee53bb1fa23ba08
EvilPrime98 <joseamin1998@gmail.com>
2025-04-04 00:04:17 +0200

    refactor terminal.js

--- src/components/settings.js
+++ src/components/settings.js
@@ -5,11 +5,11 @@ function showOptions() {
         return;
     }
 
-    const modalComponent = document.createElement("div");
-    const $btnStyle = "btn-modern-blue";
-    const $inputStyle = "btn-toggle";
-    modalComponent.classList.add("settings-modal-container");
-    modalComponent.innerHTML = `
+    const $modalComponent = document.createElement("div");
+    const btnStyle = "btn-modern-blue";
+    const inputStyle = "btn-toggle";
+    $modalComponent.classList.add("settings-modal-container");
+    $modalComponent.innerHTML = `
 
     <form class="settings-modal">
 
@@ -17,45 +17,40 @@ function showOptions() {
 
         <div class="options-group">
             <label for="visual-toggle"> Modo Visual</label>
-            <input type="checkbox" class=${$inputStyle} id="visual-toggle" name="visual-toggle">
+            <input type="checkbox" class=${inputStyle} id="visual-toggle" name="visual-toggle">
         </div>
 
         <div class="options-group">
-            <label for="ignore-arp-traffic"> Tráfico ARP (Visual) </label>
-            <input type="checkbox" class=${$inputStyle} id="ignore-arp-traffic" name="ignore-arp-traffic">
+            <label for="ignore-arp-traffic"> Ignorar Tráfico ARP (Visual) </label>
+            <input type="checkbox" class=${inputStyle} id="ignore-arp-traffic" name="ignore-arp-traffic">
         </div>
 
         <div class="options-group">
             <label for="legacy-icons"> Iconos Legado </label>
-            <input type="checkbox" class=${$inputStyle} id="legacy-icons" name="legacy-icons">
+            <input type="checkbox" class=${inputStyle} id="legacy-icons" name="legacy-icons">
         </div>
 
         <div class="options-group">
             <label for="dark-mode"> Modo Oscuro </label>
-            <input type="checkbox" class=${$inputStyle} id="dark-mode" name="dark-mode">
+            <input type="checkbox" class=${inputStyle} id="dark-mode" name="dark-mode">
         </div>
 
         <div class="options-group">
             <label for="template-option"> Template Option (Visual) </label>
-            <input type="checkbox" class=${$inputStyle} id="template-option" name="template-option">
+            <input type="checkbox" class=${inputStyle} id="template-option" name="template-option">
         </div>
         
-        <button class=${$btnStyle}>Aplicar</button>
+        <button class=${btnStyle}>Aplicar</button>
 
     </form>`;
 
-    if (ignoreArpTraffic) {
-        modalComponent.querySelector("#ignore-arp-traffic").checked = true;
-    }
+    if (ignoreArpTraffic) $modalComponent.querySelector("#ignore-arp-traffic").checked = true;
+    if (visualToggle)$modalComponent.querySelector("#visual-toggle").checked = true;
 
-    if (visualToggle) {
-        modalComponent.querySelector("#visual-toggle").checked = true;
-    }
-
-    modalComponent.querySelector("#ignore-arp-traffic").addEventListener("change", function () {ignoreArpTraffic = this.checked;});
-    modalComponent.querySelector("#visual-toggle").addEventListener("change", function () {visualToggle = this.checked;});
-    modalComponent.querySelector(`.${$btnStyle}`).addEventListener("click", closeSettingsModal);
-    document.body.appendChild(modalComponent);
+    $modalComponent.querySelector("#ignore-arp-traffic").addEventListener("change", function () {ignoreArpTraffic = this.checked;});
+    $modalComponent.querySelector("#visual-toggle").addEventListener("change", function () {visualToggle = this.checked;});
+    $modalComponent.querySelector(`.${btnStyle}`).addEventListener("click", closeSettingsModal);
+    document.body.appendChild($modalComponent);
 
 }
 
--- src/components/terminal.js
+++ src/components/terminal.js
@@ -9,79 +9,43 @@ function sendCommand(event) {
 
     if (event.key === "Enter") {
 
-        window.clearInterval(window.pingInterval); //limpiamos todos los pings en funcionamiento
-        document.querySelector(".terminal-output").innerHTML = ""; //limpiamos la salida
         const input = event.target.value.trim(); //obtenemos la entrada eliminando los espacios en blanco
-        terminalBuffer.push(input); //añadimos el comando en el buffer
-        currentCommandIndex++; //actualizamos el indice del cursos de comandos
-        document.querySelector(".terminal-component").querySelector("input").value = ""; //limpiamos la entrada
         const args = input.split(" "); //dividimos la entrada en argumentos separados por espacios
         const command = args[0]; //el primer argumento es el comando
 
-        let newoutput = "";
-
-        switch (command) {
-            case "ping":
-                command_ping(dataId, args);
-                break;
-            case "dhcp":
-                dhcp(dataId, args);
-                break;
-            case "iptables":
-                iptables(dataId, args);
-                break;
-            case "ip":
-                command_Ip(dataId, args, originIP);
-                break;
-            case "dns":
-                command_dns(dataId, args);
-                break;
-            case "dig":
-                command_dig(dataId, args);
-                break;
-            case "tcp":
-                command_tcp(dataId, args);
-                break;
-            case "http":
-                command_http(dataId, args);
-                break;
-            case "arp":
-                command_arp(dataId, args);
-                break;
-            case "apache":
-                command_apache(dataId, args);
-                break;
-            case "nano":
-                command_nano(dataId, args);
-                break;
-            case "help":
-                command_help();
-                break;
-            case "man":
-                command_man(args[1]);
-                break;
-            case "visual":
-                command_visual(args);
-                break;
-            case "test":
-                command_test(args);
-                break;
-            case "traceroute":
-                command_traceroute(dataId, args);
-                break;
-            case "exit":
+        const commandFunctions = {
+            "ping": () => command_ping(dataId, args),
+            "dhcp": () => dhcp(dataId, args),
+            "iptables": () => iptables(dataId, args),
+            "ip": () => command_Ip(dataId, args, originIP),
+            "dns": () => command_dns(dataId, args),
+            "dig": () => command_dig(dataId, args),
+            "tcp": () => command_tcp(dataId, args),
+            "http": () => command_http(dataId, args),
+            "arp": () => command_arp(dataId, args),
+            "apache": () => command_apache(dataId, args),
+            "nano": () => command_nano(dataId, args),
+            "help": () => command_help(),
+            "man": () => command_man(args[1]),
+            "visual": () => command_visual(args),
+            "test": () => command_test(args),
+            "traceroute": () => command_traceroute(dataId, args),
+            "exit": () => { 
                 event.target.value = "";
                 document.querySelector(".terminal-component").style.display = "none";
                 terminalBuffer = [];
                 currentCommandIndex = 0;
-                break;
-            default:
-                newoutput = "Command not found";
-                document.querySelector(".terminal-output").innerHTML = newoutput;
-                break;
+            }
         }
 
+        window.clearInterval(window.pingInterval); //limpiamos todos los procesos de terminal en funcionamiento
+        document.querySelector(".terminal-output").innerHTML = ""; //limpiamos la salida
+        terminalBuffer.push(input); //añadimos el comando en el buffer
+        currentCommandIndex++; //actualizamos el indice del cursor de comandos
+        document.querySelector(".terminal-component").querySelector("input").value = ""; //limpiamos la entrada
+        commandFunctions[command] ? commandFunctions[command]() : terminalMessage("Command not found"); //ejecutamos la función correspondiente
     }
+
 }
 
 function clickTerminal(event) {
@@ -93,23 +57,23 @@ function clickTerminal(event) {
 function dragTerminal(event) {
 
     event.preventDefault();
-    const terminal = event.target.closest(".terminal-component");
-    let rect = terminal.getBoundingClientRect();
+    const $terminal = event.target.closest(".terminal-component");
+    let rect = $terminal.getBoundingClientRect();
     let offsetX = event.clientX - rect.left;
     let offsetY = event.clientY - rect.top;
 
-    terminal.style.left = `${rect.left}px`;
-    terminal.style.top = `${rect.top}px`;
-    terminal.style.transform = 'none';
-    terminal.style.position = 'fixed';
+    $terminal.style.left = `${rect.left}px`;
+    $terminal.style.top = `${rect.top}px`;
+    $terminal.style.transform = 'none';
+    $terminal.style.position = 'fixed';
 
     function moveTerminal(moveEvent) {
         let x = moveEvent.clientX - offsetX;
         let y = moveEvent.clientY - offsetY;
-        let maxX = window.innerWidth - terminal.offsetWidth;
-        let maxY = window.innerHeight - terminal.offsetHeight;
-        terminal.style.left = `${Math.max(0, Math.min(x, maxX))}px`;
-        terminal.style.top = `${Math.max(0, Math.min(y, maxY))}px`;
+        let maxX = window.innerWidth - $terminal.offsetWidth;
+        let maxY = window.innerHeight - $terminal.offsetHeight;
+        $terminal.style.left = `${Math.max(0, Math.min(x, maxX))}px`;
+        $terminal.style.top = `${Math.max(0, Math.min(y, maxY))}px`;
         document.body.style.cursor = "grabbing";
     }
 
@@ -117,7 +81,7 @@ function dragTerminal(event) {
         document.body.style.cursor = "default";
         document.removeEventListener('mousemove', moveTerminal);
         document.removeEventListener('mouseup', stopDragging);
-        const input = terminal.querySelector('input');
+        const input = $terminal.querySelector('input');
         if (input) input.focus();
     }
 
@@ -127,13 +91,13 @@ function dragTerminal(event) {
 }
 
 function terminalMessage(message) {
-    const terminal = document.querySelector(".terminal-component");
-    if (terminal.style.display === "none") return;
-    const output = document.querySelector(".terminal-output");
-    const messageElement = document.createElement("p");
-    messageElement.className = "terminal-message";
-    messageElement.innerHTML = message;
-    output.appendChild(messageElement);
+    const $terminal = document.querySelector(".terminal-component");
+    const $output = document.querySelector(".terminal-output");
+    const $messageElement = document.createElement("p");
+    if ($terminal.style.display === "none") return;
+    $messageElement.className = "terminal-message";
+    $messageElement.innerHTML = message;
+    $output.appendChild($messageElement);
     //terminal.scrollTop = output.scrollHeight;
 }
 
@@ -167,21 +131,18 @@ function terminalKeyboard(event) {
 function getRoutingRules(routerObjectid, targetinterface) {
 
     const $routerObject = document.getElementById(routerObjectid);
-    const routingTable = $routerObject.querySelector(".routing-table").querySelector("table");
-    const rows = routingTable.querySelectorAll("tr");
+    const $routingTable = $routerObject.querySelector(".routing-table").querySelector("table");
+    const $rows = $routingTable.querySelectorAll("tr");
     const rules = [];
 
-    for (let i = 4; i < rows.length; i++) {
-        let row = rows[i];
-        let cells = row.querySelectorAll("td");
-        let destination = cells[0].innerHTML.trim();
-        let netmask = cells[1].innerHTML.trim();
-        let interface = cells[3].innerHTML.trim();
-        let nextHop = cells[4].innerHTML.trim();
-
-        if (interface === targetinterface && nextHop !== "0.0.0.0") {
-            rules.push(`ip route add ${destination}/${netmaskToCidr(netmask)} via ${nextHop}`);
-        }
+    for (let i = 4; i < $rows.length; i++) {
+        let $row = $rows[i];
+        let $cells = $row.querySelectorAll("td");
+        let destination = $cells[0].innerHTML.trim();
+        let netmask = $cells[1].innerHTML.trim();
+        let interface = $cells[3].innerHTML.trim();
+        let nextHop = $cells[4].innerHTML.trim();
+        if (interface === targetinterface && nextHop !== "0.0.0.0") rules.push(`ip route add ${destination}/${netmaskToCidr(netmask)} via ${nextHop}`);
     }
 
     return rules;
@@ -189,34 +150,34 @@ function getRoutingRules(routerObjectid, targetinterface) {
 
 async function minimizeTerminal() {
     return new Promise(resolve => {
-        const terminal = document.querySelector(".terminal-component");
-        if (!terminal || terminal.style.display === "none") return resolve();
-        const rect = terminal.getBoundingClientRect();
+        const $terminal = document.querySelector(".terminal-component");
+        if (!$terminal || $terminal.style.display === "none") return resolve();
+        const rect = $terminal.getBoundingClientRect();
         const targetWidth = rect.width * 0.3;
         const targetHeight = rect.height * 0.3;
         const windowHeight = window.innerHeight;
-        terminal.style.transition = "all 1s ease-in-out";
-        terminal.style.width = `${targetWidth}px`;
-        terminal.style.height = `${targetHeight}px`;
-        terminal.style.top = `${windowHeight - targetHeight}px`;
-        terminal.style.left = "100%";
-        terminal.style.transform = "translate(-100%, 0)";
-        terminal.addEventListener("transitionend", resolve, { once: true });
+        $terminal.style.transition = "all 1s ease-in-out";
+        $terminal.style.width = `${targetWidth}px`;
+        $terminal.style.height = `${targetHeight}px`;
+        $terminal.style.top = `${windowHeight - targetHeight}px`;
+        $terminal.style.left = "100%";
+        $terminal.style.transform = "translate(-100%, 0)";
+        $terminal.addEventListener("transitionend", resolve, { once: true });
     });
 }
 
 async function maximizeTerminal() {
     return new Promise(resolve => {
-        const terminal = document.querySelector(".terminal-component");
-        if (!terminal || terminal.style.display === "none") return resolve();
-        terminal.style.transition = "all 1s ease-in-out";
-        terminal.style.width = "1000px";
-        terminal.style.height = "500px";
-        terminal.style.top = "40%";
-        terminal.style.left = "50%";
-        terminal.style.transform = "translate(-50%, -50%)";
-        terminal.addEventListener("transitionend", () => {
-            terminal.style.transition = "none";
+        const $terminal = document.querySelector(".terminal-component");
+        if (!$terminal || $terminal.style.display === "none") return resolve();
+        $terminal.style.transition = "all 1s ease-in-out";
+        $terminal.style.width = "1000px";
+        $terminal.style.height = "500px";
+        $terminal.style.top = "40%";
+        $terminal.style.left = "50%";
+        $terminal.style.transform = "translate(-50%, -50%)";
+        $terminal.addEventListener("transitionend", () => {
+            $terminal.style.transition = "none";
             resolve();
         }, { once: true });
     });
