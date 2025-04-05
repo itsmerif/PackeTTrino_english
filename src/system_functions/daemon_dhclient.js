91f53d4607091dff3247d58427f11a945abb407f
EvilPrime98 <joseamin1998@gmail.com>
2025-04-05 15:50:15 +0200

    add dhclient daemon

---
+++ src/system_functions/daemon_dhclient.js
@@ -0,0 +1,43 @@
+async function dhclient_service(networkObjectId, packet) {
+
+    const $networkObject = document.getElementById(networkObjectId);
+    const networkObjectMac = $networkObject.getAttribute("data-mac");
+    const switchId = $networkObject.getAttribute("data-switch");
+
+    if ($networkObject.getAttribute("data-dhcp") === "false") return; 
+
+    if (packet.type === "offer") {
+
+        if (packet.chaddr === networkObjectMac) {
+
+            dhcpDiscoverFlag = true;
+
+            let newPacket = new dhcpRequest(
+                networkObjectMac, //origin mac
+                packet.yiaddr, //requested ip
+                packet.siaddr, //server ip
+                networkObjectId //hostname
+            );
+
+            newPacket.destination_mac = packet.origin_mac;
+            newPacket.yiaddr = packet.yiaddr;
+            newPacket.giaddr = packet.giaddr;
+            newPacket.chaddr = packet.chaddr;
+
+            addPacketTraffic(newPacket);
+            await switchProcessor(switchId, networkObjectId, newPacket);
+
+        }
+
+    }
+
+    if (packet.type === "ack") {
+
+        if (packet.chaddr === networkObjectMac) {
+            dhcpRequestFlag = true;
+            setDhcpInfo(networkObjectId, packet);
+        }
+
+    }
+
+}
