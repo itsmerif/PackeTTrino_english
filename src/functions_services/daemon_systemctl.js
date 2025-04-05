b448d559c98c20081186b0a164c382ca386c5fef
EvilPrime98 <joseamin1998@gmail.com>
2025-04-05 23:07:17 +0200

    added systemctl list-units

--- src/functions_services/daemon_systemctl.js
+++ src/functions_services/daemon_systemctl.js
@@ -3,17 +3,22 @@ function command_systemctl(networkObjectId, args) {
     const option = args[1];
     const service = args[2];
 
-    const validOptions = ["start", "restart", "stop", "status"];
+    const validOptions = ["start", "restart", "stop", "status", "list-units"];
     const validServices = ["dhcpd", "apache", "dhclient", "dhcrelay", "resolved", "named"];
-
-    if (!validServices.includes(service)) {
-        terminalMessage("Error: Servicio no reconocido.");
-        return;
-    }
 
     if (!validOptions.includes(option)) {
         terminalMessage("Error: Opción no reconocida.");
         terminalMessage("Sintaxis: systemctl [start|restart|stop|status] &lt;nombre del servicio&gt;");
+        return;
+    }
+
+    if (option === "list-units") {
+        listallServices(networkObjectId);
+        return;
+    };
+
+    if (!validServices.includes(service)) {
+        terminalMessage("Error: Servicio no reconocido.");
         return;
     }
 
@@ -61,5 +66,24 @@ function daemonManager(networkObjectId, service, option) {
     }
 
     stateFunctions[option]();
+
+}
+
+function listallServices(networkObjectId) {
+
+    const $networkObject = document.getElementById(networkObjectId);
+    const availableServices = ["dhcpd", "apache", "dhclient", "dhcrelay", "resolved", "named"];
+
+    availableServices.forEach(service => {
+
+        let isServiceAvailable = $networkObject.getAttribute(service) !== null;
+
+        if (isServiceAvailable) {
+            let isServiceActive = $networkObject.getAttribute(service) === "true";
+            let status = (isServiceActive) ? "<span style='color:#4ade80;'> Activo (running) </span>" : "<span style='color:red;'> Inactivo (dead) </span>";
+            terminalMessage(`${service}.service: ${status}`);
+        }
+
+    });
 
 }
