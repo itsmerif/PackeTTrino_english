76a3075e77697015461e71f2c9608884e18d49c7
EvilPrime98 <joseamin1998@gmail.com>
2025-04-05 18:37:33 +0200

    add dhcrelay daemon

---
+++ src/functions_services/daemon_dhcrelay.js
@@ -0,0 +1,74 @@
+async function dhcrelay_service(agentObjectId, packet) {
+
+    const $agentObject = document.getElementById(agentObjectId);
+    const agentObjectMac = $agentObject.getAttribute("data-mac");
+    const agentObjectIp = $agentObject.getAttribute("data-ip");
+    const mainServer = $agentObject.getAttribute("data-main-server");
+    const defaultGateway = $agentObject.getAttribute("data-gateway");
+
+    if (packet.destination_ip = "255.255.255.255") {
+        
+        if (packet.type === "discover") {
+
+            packet.chaddr = packet.origin_mac;
+            packet.destination_ip = mainServer;
+            packet.origin_mac = agentObjectMac;
+            packet.giaddr = agentObjectIp;
+            packet.destination_mac = "";
+            packet.origin_ip = agentObjectIp;
+
+            let defaultGatewayMac = isIpInARPTable(agentObjectId, defaultGateway);
+
+            if (!defaultGatewayMac) {
+                buffer[agentObjectId] = packet;
+                await arpResolve(agentObjectId, defaultGateway);
+                return;
+            }
+
+            packet.destination_mac = defaultGatewayMac;
+            addPacketTraffic(packet);
+            await switchProcessor(switchId, agentObjectId, packet);
+            return;
+
+        }
+
+        if (packet.type === "request") {
+            if (packet.giaddr !== agentObjectIp) return;
+            packet.origin_ip = agentObjectIp;
+            packet.destination_ip = mainServer;
+            packet.origin_mac = agentObjectMac;
+            packet.destination_mac = isIpInARPTable(agentObjectId, defaultGateway);
+            addPacketTraffic(packet);
+            await switchProcessor(switchId, agentObjectId, packet);
+            return;
+        }
+
+    }
+
+    if (packet.destination_ip === agentObjectIp) {
+
+        if (packet.type === "offer") {
+            if (packet.giaddr !== agentObjectIp) return;
+            packet.destination_mac = packet.ciaddr;
+            packet.destination_ip = "255.255.255.255";
+            packet.origin_mac = agentObjectMac;
+            packet.destination_mac = packet.chaddr;
+            addPacketTraffic(packet);
+            await switchProcessor(switchId, agentObjectId, packet);
+            return;
+        }
+
+        if (packet.type === "ack") {
+            if (packet.giaddr === agentObjectMac) return;
+            packet.origin_ip = agentObjectIp;
+            packet.destination_ip = "255.255.255.255";
+            packet.origin_mac = agentObjectMac;
+            packet.destination_mac = packet.chaddr;
+            addPacketTraffic(packet);
+            await switchProcessor(switchId, agentObjectId, packet);
+            return;
+        }
+
+    }
+
+}
--- src/processors/dhcpRelayProc.js
+++ src/processors/dhcpRelayProc.js
@@ -1,134 +1,62 @@
-async function packetProcessor_dhcp_relay_server(switchId, serverObjectId, packet) {
+async function packetProcessor_dhcp_relay_server(switchId, agentObjectId, packet) {
 
-    if (visualToggle) await visualize(switchId, serverObjectId, packet);
+    if (visualToggle) await visualize(switchId, agentObjectId, packet);
 
-    //cortafuegos
+    if (!firewallProcessorHost(agentObjectId, packet)) return;
 
-    if (!firewallProcessorHost(serverObjectId, packet)) return;
+    const $agentObject = document.getElementById(agentObjectId);
+    const agentObjectMac = $agentObject.getAttribute("data-mac");
+    const agentObjectIp = $agentObject.getAttribute("data-ip");
 
-    const $serverObject = document.getElementById(serverObjectId);
-    const serverObjectMac = $serverObject.getAttribute("data-mac");
-    const serverObjectIp = $serverObject.getAttribute("data-ip");
-    const mainServer = $serverObject.getAttribute("data-main-server");
-    const defaultGateway = $serverObject.getAttribute("data-gateway");
+    if (packet.protocol === "arp" && packet.type === "request") {
 
-    if (packet.destination_ip === serverObjectIp) { //el paquete es para mi
+        if (packet.destination_ip !== agentObjectIp) return;
 
-        //comportamiento de pc
+        addARPEntry(agentObjectId, packet.origin_ip, packet.origin_mac);
 
-        if (packet.protocol === "arp" && packet.type === "request") {
+        let newPacket = new ArpReply(agentObjectIp, packet.origin_ip, agentObjectMac, packet.origin_mac);
 
-            addARPEntry(serverObjectId, packet.origin_ip, packet.origin_mac);
+        addPacketTraffic(newPacket);
+        await switchProcessor(switchId, agentObjectId, newPacket);
 
-            let newPacket = new ArpReply(serverObjectIp, packet.origin_ip, serverObjectMac, packet.origin_mac);
-
-            addPacketTraffic(newPacket);
-            await switchProcessor(switchId, serverObjectId, newPacket);
-
-            return;
-        }
-
-        if (packet.protocol === "arp" && packet.type === "reply") {
-
-            addARPEntry(serverObjectId, packet.origin_ip, packet.origin_mac);
-
-            let bufferPacket = buffer[serverObjectId];
-
-            if (bufferPacket) {
-                bufferPacket.destination_mac = isIpInARPTable(serverObjectId, packet.origin_ip);
-                delete buffer[serverObjectId];
-                addPacketTraffic(bufferPacket);
-                await switchProcessor(switchId, serverObjectId, bufferPacket);
-            }
-
-            return;
-        }
-
-        if (packet.protocol === "icmp" && packet.type === "request") {
-
-            let newPacket = new IcmpEchoReply(serverObjectIp, packet.origin_ip, serverObjectMac, packet.origin_mac);
-            addPacketTraffic(newPacket);
-            await switchProcessor(switchId, serverObjectId, newPacket);
-
-            return;
-        }
-
-        if (packet.protocol === "icmp" && packet.type === "reply") {
-        
-            icmpFlag = true;
-            return;
-
-        }
-
-        //comportamiento de agente de retransmision dhcp
-
-        if (packet.protocol === "dhcp" && packet.type === "offer") { //oferta del server principal
-            if (packet.giaddr !== serverObjectIp) return; //comprobamos si el offer está dirigido al agente
-            //reenviamos el paquete al cliente que lo solicitó
-            packet.destination_mac = packet.ciaddr;
-            packet.destination_ip = "255.255.255.255";
-            packet.origin_mac = serverObjectMac;
-            packet.destination_mac = packet.chaddr;
-            addPacketTraffic(packet);
-            await switchProcessor(switchId, serverObjectId, packet);
-            return;
-        }
-
-        if (packet.protocol === "dhcp" && packet.type === "ack") {
-            if (packet.giaddr === serverObjectMac) return; //comprobamos si el ack está dirigido al agente
-            //cambiamos los campos del paquete
-            packet.origin_ip = serverObjectIp;
-            packet.destination_ip = "255.255.255.255";
-            packet.origin_mac = serverObjectMac;
-            packet.destination_mac = packet.chaddr;
-            addPacketTraffic(packet);
-            await switchProcessor(switchId, serverObjectId, packet);
-            return;
-        }
-
+        return;
     }
 
-    if (packet.destination_ip = "255.255.255.255") {
-        
-        if (packet.protocol === "dhcp" && packet.type === "discover") { //peticion de descubrimiento dhcp, la mandamos al server
+    if (packet.protocol === "arp" && packet.type === "reply") {
 
-            //hago cambios en el paquete para que se envie al servidor
-            packet.chaddr = packet.origin_mac;
-            packet.destination_ip = mainServer;
-            packet.origin_mac = serverObjectMac;
-            packet.giaddr = serverObjectIp;
-            packet.destination_mac = "";
-            packet.origin_ip = serverObjectIp;
+        if (packet.destination_ip !== agentObjectIp) return;
 
-            let defaultGatewayMac = isIpInARPTable(serverObjectId, defaultGateway);
+        addARPEntry(agentObjectId, packet.origin_ip, packet.origin_mac);
 
-            if (!defaultGatewayMac) { //no tenemos la ip de la puerta de enlace en nuestra tabla de arp, lo guardamos en el buffer y enviamos un ARP primero
-                buffer[serverObjectId] = packet;
-                await arpResolve(serverObjectId, defaultGateway);
-                return;
-            }
+        let bufferPacket = buffer[agentObjectId];
 
-            packet.destination_mac = defaultGatewayMac;
-            addPacketTraffic(packet);
-            await switchProcessor(switchId, serverObjectId, packet);
-            return;
-
+        if (bufferPacket) {
+            bufferPacket.destination_mac = isIpInARPTable(agentObjectId, packet.origin_ip);
+            delete buffer[agentObjectId];
+            addPacketTraffic(bufferPacket);
+            await switchProcessor(switchId, agentObjectId, bufferPacket);
         }
 
-
-        if (packet.protocol === "dhcp" && packet.type === "request") {
-            if (packet.giaddr !== serverObjectIp) return; //comprobamos si el request está dirigido al agente
-            //cambiamos los campos del paquete
-            packet.origin_ip = serverObjectIp;
-            packet.destination_ip = mainServer;
-            packet.origin_mac = serverObjectMac;
-            packet.destination_mac = isIpInARPTable(serverObjectId, defaultGateway);
-            addPacketTraffic(packet);
-            await switchProcessor(switchId, serverObjectId, packet);
-            return;
-        }
-
+        return;
     }
 
+    if (packet.protocol === "icmp" && packet.type === "request") {
+
+        if (packet.destination_ip !== agentObjectIp) return;
+
+        let newPacket = new IcmpEchoReply(agentObjectIp, packet.origin_ip, agentObjectMac, packet.origin_mac);
+        addPacketTraffic(newPacket);
+        await switchProcessor(switchId, agentObjectId, newPacket);
+
+        return;
+    }
+
+    if (packet.protocol === "icmp" && packet.type === "reply") {
+        if (packet.destination_ip !== agentObjectIp) return;
+        icmpFlag = true;
+        return;
+    }
+
+    if (packet.protocol === "dhcp") dhcrelay_service(agentObjectId, packet);
 
 }
