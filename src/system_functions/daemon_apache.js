8b6e4635166612a2254acae23ccaaff93542df59
EvilPrime98 <joseamin1998@gmail.com>
2025-04-05 15:50:05 +0200

    add apache daemon

---
+++ src/system_functions/daemon_apache.js
@@ -0,0 +1,17 @@
+async function apache_service(networkObjectId, packet) {
+
+    const $networkObject = document.getElementById(networkObjectId);
+    const networkObjectIp = $networkObject.getAttribute("data-ip");
+    const networkObjectMac = $networkObject.getAttribute("data-mac");
+    const switchId = $networkObject.getAttribute("data-switch");
+
+    if (packet.destination_ip !== networkObjectIp) return;
+
+    if ($networkObject.getAttribute("web-server") === "off") return;
+
+    let newPacket = new httpReply(networkObjectIp, packet.origin_ip, networkObjectMac, packet.origin_mac);
+    newPacket.body = $networkObject.getAttribute("web-content");
+    addPacketTraffic(newPacket);
+    await switchProcessor(switchId, networkObjectId, newPacket);
+    
+}
