23fa82f106c93073490fb4127c00763747fc9c22
EvilPrime98 <joseamin1998@gmail.com>
2025-04-06 12:26:55 +0200

    add daemon named

---
+++ src/functions_services/daemon_named.js
@@ -0,0 +1,116 @@
+async function named_service(networkObjectId, packet) {
+
+    const $networkObject = document.getElementById(networkObjectId);
+    const networkObjectMac = $networkObject.getAttribute("data-mac");
+    const networkObjectIp = $networkObject.getAttribute("data-ip");
+    const isRecursive = $networkObject.getAttribute("recursion");
+
+    if (packet.destination_mac !== serverObjectMac || packet.destination_ip !== serverObjectIp) return;
+
+    let newPacket = new dnsReply(serverObjectIp, packet.origin_ip, serverObjectMac, packet.origin_mac, packet.query, ""); //inicializamos el paquete sin respuesta
+    let answer;
+
+    if (packet.answer_type === "SOA") {
+        let authority_domain;
+        [authority_domain, answer] = dns_SOA_Request_Proc(serverObjectId, packet);
+        newPacket.answer_type = "SOA";
+        newPacket.answer = answer;
+        newPacket.authority_domain = authority_domain;
+        if (answer) newPacket.authority = "1";
+    }
+
+    if (packet.answer_type === "A") {
+        answer = dns_A_Request_Proc(serverObjectId, packet);
+        if (!answer && isRecursive !== "false") answer = await recursiveDNSResolve(packet.query);
+        newPacket.answer_type = "A";
+        newPacket.answer = answer;
+    }
+
+    addPacketTraffic(newPacket);
+    await switchProcessor(switchId, serverObjectId, newPacket);
+    return;
+
+}
+
+
+function dns_SOA_Request_Proc(serverObjectId, packet) {
+
+    const $serverObject = document.getElementById(serverObjectId);
+    const dnsTable = $serverObject.querySelector(".dns-table").querySelector("table");
+    const records = dnsTable.querySelectorAll("tr");
+    let recordIndex;
+    let query = packet.query; // google.com
+    let targetDomain;
+    let response = [false, false];
+
+    //nos aseguramos de que el dominio que estamos buscando sea un FQDN
+
+    if (!query.endsWith(".")) query = query + "."; // google.com.
+    query = query.split("."); // ["google", "com", ""]
+
+    //buscamos en la tabla de registros DNS en cada nivel de autoridad
+
+    for (let i = 0; i < query.length; i++) {
+        targetDomain = query.slice(i).join("."); // "google.com.", "com.", "."
+        recordIndex = 1;
+        while (recordIndex < records.length && !response[0]) {
+            let row = records[recordIndex];
+            let cells = row.querySelectorAll("td");
+            let domain = cells[0].innerHTML;
+            let type = cells[1].innerHTML;
+            let value = cells[2].innerHTML;
+            if (targetDomain === domain && type === "SOA") response = [targetDomain, value];
+            recordIndex++;
+        }
+    }
+
+    return response;
+}
+
+function dns_A_Request_Proc(serverObjectId, packet) {
+
+    const $serverObject = document.getElementById(serverObjectId);
+    const dnsTable = $serverObject.querySelector(".dns-table").querySelector("table");
+    const records = dnsTable.querySelectorAll("tr");
+    let response = false;
+    let recordIndex;
+    let query = packet.query; // www.google.com
+
+    //nos aseguramos de que el dominio que estamos buscando sea un FQDN
+
+    if (!query.endsWith(".")) query = query + "."; // google.com.
+
+    //buscamos en la tabla de registros
+
+    recordIndex = 1;
+
+    while (recordIndex < records.length && !response) {
+        let row = records[recordIndex];
+        let cells = row.querySelectorAll("td");
+        let domain = cells[0].innerHTML;
+        let type = cells[1].innerHTML;
+        let value = cells[2].innerHTML;
+        if (domain === query && type === "CNAME") query = value // www.google.com -> google.com
+        if (domain === query && type === "A") response = value;
+        recordIndex++;
+    }
+
+    return response;
+
+}
+
+async function recursiveDNSResolve(domain) {
+    try {
+        const response = await fetch("https://dns.google.com/resolve?name=" + domain + "&type=A");
+        const reply = await response.json();
+        let answer = [];
+        for (let i = 0; i < reply.Answer.length; i++) {
+            answer.push(reply.Answer[i].data);
+        }
+        console.log(answer);
+        return answer;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
