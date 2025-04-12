8507bf78745eb5509dcc2132830d29265a324171
EvilPrime98 <joseamin1998@gmail.com>
2025-04-12 10:09:06 +0200

    arp resolve improved scaling for multiple interfaces

--- src/system/kernel_arp.js
+++ src/system/kernel_arp.js
@@ -1,8 +1,8 @@
-async function arpResolve(networkObjectId, destinationIp)  {
+async function arpResolve(networkObjectId, destinationIp, interface = "enp0s3")  {
     const $networkObject = document.getElementById(networkObjectId);
-    const switchId = $networkObject.getAttribute("data-switch-enp0s3");
-    const originIp = $networkObject.getAttribute("ip-enp0s3");
-    const originMac = $networkObject.getAttribute("mac-enp0s3");
+    const switchId = $networkObject.getAttribute("data-switch-" + interface);
+    const originIp = $networkObject.getAttribute("ip-" + interface);
+    const originMac = $networkObject.getAttribute("mac-" + interface);
     let packet = new ArpRequest(originIp, destinationIp, originMac);
     addPacketTraffic(packet);
     await switchProcessor(switchId, networkObjectId, packet);
