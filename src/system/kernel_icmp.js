6ff567ee6b8a2f5a1e1c8714ac43ad5fa11848bb
EvilPrime98 <joseamin1998@gmail.com>
2025-04-15 16:00:06 +0200

    fix and improved ping simulation function

--- src/system/kernel_icmp.js
+++ src/system/kernel_icmp.js
@@ -113,29 +113,38 @@ async function traceroute(dataId, destination, numeric = false) {
 async function icmpRequestPacketGenerator(networkObjectId, switchId, originIp, destinationIp) {
 
     const $networkObject = document.getElementById(networkObjectId);
-    const isSameNetwork = getNetwork(originIp, $networkObject.getAttribute("netmask-enp0s3")) === getNetwork(destinationIp, $networkObject.getAttribute("netmask-enp0s3"));
+    const networkObjectNetmask =  $networkObject.getAttribute("netmask-enp0s3");
+    const isSameNetwork = getNetwork(originIp, networkObjectNetmask) === getNetwork(destinationIp, networkObjectNetmask);
+    const isRouter = networkObjectId.startsWith("router-");
     const networkObjectMac = $networkObject.getAttribute("mac-enp0s3"); 
     let packet = new IcmpEchoRequest(originIp, destinationIp, networkObjectMac, "");
     let destination_mac;
 
     if (!isSameNetwork) { 
-
-        const defaultGateway = $networkObject.getAttribute("data-gateway");
-
-        if (!defaultGateway) throw new Error("Error: Puerta de Enlace Predeterminada No Configurada");
         
-        const defaultGatewayMac = isIpInARPTable(networkObjectId, defaultGateway);
+        if (!isRouter) {
 
-        if (!defaultGatewayMac) {
-            buffer[networkObjectId] = packet;
-            await arpResolve(networkObjectId, defaultGateway);
+            const defaultGateway = $networkObject.getAttribute("data-gateway");
+            if (!defaultGateway) throw new Error("Error: Puerta de Enlace Predeterminada No Configurada");          
+            const defaultGatewayMac = isIpInARPTable(networkObjectId, defaultGateway);
+    
+            if (!defaultGatewayMac) {
+                buffer[networkObjectId] = packet;
+                await arpResolve(networkObjectId, defaultGateway);
+                return;
+            }
+    
+            packet.destination_mac = defaultGatewayMac;
+            addPacketTraffic(packet);
+            await switchProcessor(switchId, networkObjectId, packet);
             return;
-        }
 
-        packet.destination_mac = defaultGatewayMac;
-        addPacketTraffic(packet);
-        await switchProcessor(switchId, networkObjectId, packet);
-        return;
+        } else {
+
+            await routing(networkObjectId, packet);
+            return;
+
+        }
 
     }
 
--- src/utilities/ping_util.js
+++ src/utilities/ping_util.js
@@ -18,3 +18,35 @@ async function command_ping(dataId, args) {
     await ping(dataId, args);
 
 }
+
+async function icmpTryoutProcess(id) {
+
+    const $networkObject = document.getElementById(id);
+    const $board = document.querySelector(".board");
+    
+    const objectIp = getAvailableIps($networkObject.id)[0];
+
+    if (!objectIp) {
+        bodyComponent.render(popupMessage("<span>Error: </span> No se ha encontrado la IP del objeto " + id));
+        return;
+    }
+
+    if (icmpTryoutObject === "") {        
+        icmpTryoutObject = id;
+        createPacketIndicator(id);
+        return;
+    }
+
+    createPacketIndicator(id);
+
+    visualToggle =  true;
+
+    await ping(icmpTryoutObject, ["ping", objectIp]);
+
+    visualToggle = false;
+    
+    $board.querySelectorAll(".pack-cursor").forEach(cursor => {cursor.remove();});
+
+    icmpTryoutObject = "";
+
+}
