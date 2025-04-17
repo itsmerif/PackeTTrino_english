8d135a6ad254847a66b7e2ff053c7c2ebff553fc
EvilPrime98 <joseamin1998@gmail.com>
2025-04-18 00:52:22 +0200

    apache fix

--- src/utilities/apache_util.js
+++ src/utilities/apache_util.js
@@ -1,29 +1,31 @@
-function command_apache(id, args) {
+function command_apache(networkObjectId, args) {
 
-    const $networkObject = document.getElementById(id);
+    const $networkObject = document.getElementById(networkObjectId);
     const networkObjectIp = $networkObject.getAttribute("ip-enp0s3");
     const networkObjectMac = $networkObject.getAttribute("mac-enp0s3");
     const networkObjectNetmask = $networkObject.getAttribute("netmask-enp0s3");
 
     if (!networkObjectIp || !networkObjectMac || !networkObjectNetmask) {
-        terminalMessage("Error: No se ha configurado el equipo.");
+        terminalMessage("Error: No se ha configurado el equipo.", networkObjectId);
         return;
     }
 
     if (args[1] === "on") {
         $networkObject.setAttribute("apache", "on");
-        terminalMessage("Servidor web encendido correctamente.");
+        terminalMessage("Servidor web encendido correctamente.", networkObjectId);
         return;
     }
 
     if (args[1] === "off") {
         $networkObject.setAttribute("apache", "off");
-        terminalMessage("Servidor web apagado correctamente.");
+        terminalMessage("Servidor web apagado correctamente.", networkObjectId);
         return;
     }
 
     if (args[1] === "status") {
-        ($networkObject.getAttribute("apache") === "on") ? terminalMessage("Servidor web encendido.") : terminalMessage("Servidor web apagado.");
+        ($networkObject.getAttribute("apache") === "on") 
+        ? terminalMessage("Servidor web encendido.", networkObjectId)
+        : terminalMessage("Servidor web apagado.", networkObjectId);
         return;
     }
 
