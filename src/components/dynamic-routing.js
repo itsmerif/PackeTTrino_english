e9ac56001f2eb372146e45422c72fa3836fc8f5d
EvilPrime98 <joseamin1998@gmail.com>
2025-04-26 08:46:25 +0200

    FEAT(dynamic routing button): ahora el boton de enrutamiento dinamico es un svg, con la misma forma que el resto de botones del panel. Un poco de refactorizacion al estilo del button.

--- src/components/dynamic-routing.js
+++ src/components/dynamic-routing.js
@@ -7,7 +7,7 @@ function dynamicRoutingButton() {
     $dynamicRouting.draggable = false;
 
     $dynamicRouting.innerHTML = `
-        <img src="./assets/panel/dynrouter.png" alt="dynrouting" draggable="false" style="width: 100%; height: 100%;">
+        <img src="./assets/panel/dynrouter.svg" alt="dynrouting" draggable="false" style="width: 100%; height: 100%;">
         <div class="pulse"></div>
         <div class="radar-line"></div>
     `;
