f8843e9385febb60f6e519b3167d3be3324c69a1
EvilPrime98 <joseamin1998@gmail.com>
2025-05-17 22:33:36 +0200

    FEAT se ha añadido un slider que controla la velocidad de animacion

--- src/components/menus/generalSettingsMenu.js
+++ src/components/menus/generalSettingsMenu.js
@@ -1,38 +1,46 @@
 function GeneralOptions() {
 
     const $generalOptions = document.createElement("form");
-    const btnStyle = "btn-modern-blue";
-    const inputStyle = "btn-toggle";
 
     $generalOptions.classList.add("settings-modal", "modal", "draggable-modal");
 
     $generalOptions.innerHTML = `
 
-        <div class="window-frame"> <h1> Opciones Generales </h1> </div>
+        <div class="window-frame"> <p> Opciones Generales </p> </div>
 
         <div class="options-group">
             <label for="visual-toggle"> Modo Visual</label>
-            <input type="checkbox" class=${inputStyle} id="visual-toggle" name="visual-toggle">
+            <input type="checkbox" class="btn-toggle" id="visual-toggle" name="visual-toggle">
         </div>
 
         <div class="options-group">
-            <label for="ignore-arp-traffic"> No mostrar paquetes ARP </label>
-            <input type="checkbox" class=${inputStyle} id="ignore-arp-traffic" name="ignore-arp-traffic">
+            <label for="ignore-arp-traffic"> No Mostrar Tráfico ARP </label>
+            <input type="checkbox" class="btn-toggle" id="ignore-arp-traffic" name="ignore-arp-traffic">
         </div>
 
         <div class="options-group">
             <label for="dark-mode"> Modo Oscuro </label>
-            <input type="checkbox" class=${inputStyle} id="dark-mode" name="dark-mode">
+            <input type="checkbox" class="btn-toggle" id="dark-mode" name="dark-mode">
         </div>
         
-        <button class=${btnStyle}>Cerrar</button>
+        <div class="options-group">
+            <label for="visual-speed"> Velocidad de Animación </label>
+            <input type="range" id="visual-speed" name="visual-speed" min="100" max="1000">
+            <p id="visual-speed-value">300</p><span>ms</span>
+        </div>
+
+        <button class="btn-modern-blue">Cerrar</button>
     `;
 
     $generalOptions.querySelector("#ignore-arp-traffic").addEventListener("change", function () { ignoreArpTraffic = this.checked; });
     $generalOptions.querySelector("#visual-toggle").addEventListener("change", function () { visualToggle = this.checked; });
     $generalOptions.querySelector("#dark-mode").addEventListener("change", activateDarkMode);
-    $generalOptions.querySelector(`.${btnStyle}`).addEventListener("click", generalOptionsHandler);
+    $generalOptions.querySelector(".btn-modern-blue").addEventListener("click", generalOptionsHandler);
     $generalOptions.querySelector(".window-frame").addEventListener("mousedown", dragModal);
+    $generalOptions.querySelector("#visual-speed").addEventListener("input", function () { 
+        visualSpeed = this.value; 
+        $generalOptions.querySelector("#visual-speed-value").innerHTML = this.value;
+    });
 
     return $generalOptions;
 
