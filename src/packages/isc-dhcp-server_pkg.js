9ba844e140c73a4ef660c1bb06b72561aa23d659
EvilPrime <joseamin1998@gmail.com>
2025-05-23 18:30:31 +0200

    FEAT se ha añadido el fichero isc-dhcp-server para gestionar las interfaces de escucha en el servicio dhcpd

--- src/packages/isc-dhcp-server_pkg.js
+++ src/packages/isc-dhcp-server_pkg.js
@@ -1,32 +1,36 @@
 function installDhcpd($networkObject) {
 
     const networkObjectId = $networkObject.id;
-
-    terminalMessage("Instalando DHCP Server...", networkObjectId);
-
     const $advancedOptions = $networkObject.querySelector(".advanced-options-modal");
     const attr = (attribute, value) => $networkObject.setAttribute(attribute, value);
     const append = (...nodes) => nodes.forEach(node => $networkObject.appendChild(node));
     const addOption = (...nodes) => nodes.forEach(node => $advancedOptions.appendChild(node));
     const networkObjectFileSystem = new FileSystem($networkObject);
 
+    terminalMessage("Instalando DHCP Server...", networkObjectId);
+
+    //directorios y archivos
     networkObjectFileSystem.mkdir("dhcp", ["etc"]);
     networkObjectFileSystem.touch("dhcpd.conf", ["etc", "dhcp"]);
+    networkObjectFileSystem.mkdir("default", ["etc"]);
+    networkObjectFileSystem.touch("isc-dhcp-server", ["etc", "default"]);
 
-    append(dhcpTable());
-
+    //atributos del servidor DHCP
     attr("dhcpd", "true");
+    attr("dhcp-listen-on-interfaces", "");
+    attr("data-interval", "false");
+
+    //atributos del servicio DHCP
     attr("data-range-start", "");
     attr("data-range-end", "");
-    attr("offer-gateway", "");
-    attr("offer-netmask", "");
-    attr("offer-dns", "");
-    attr("offer-lease-time", "");
-    attr("data-interval", "false");
-    attr("ip-reservations", `{}`);
-    attr("dhcp-listen-on-interfaces", "");
-    
-    addOption(leasesTableOptionButton(), dhcpServerConfig());
+    attr("dhcp-offer-gateway", "");
+    attr("dhcp-offer-netmask", "");
+    attr("dhcp-offer-dns", "");  
+    attr("dhcp-offer-lease-time", "");
+    attr("dhcp-reservations", `{}`);
+
+    addOption(leasesTableOptionButton(), dhcpServerConfig()); //<-- se añaden opciones a las opciones avanzadas
+    append(dhcpTable()); //<-- se añade la tabla de DHCP
 
     terminalMessage("DHCP Server instalado correctamente.", networkObjectId);
 }
@@ -43,22 +47,30 @@ function uninstallDhcpd(networkObjectId) {
     const remOption = (...options) => options.forEach(option => $advancedOptions.querySelector("#" + option).remove());
     const networkObjectFileSystem = new FileSystem($networkObject);
 
+    //directorios y archivos
     networkObjectFileSystem.rmdir("dhcp", ["etc"]);
+    networkObjectFileSystem.rmdir("default", ["etc"]);
     
+    //atributos del servidor DHCP
     rattr(
         "dhcpd", 
         "data-range-start", 
         "data-range-end", 
-        "offer-gateway", 
-        "offer-netmask", 
-        "offer-dns", 
-        "offer-lease-time", 
+        "dhcp-offer-gateway", 
+        "dhcp-offer-netmask", 
+        "dhcp-offer-dns", 
+        "dhcp-offer-lease-time", 
         "data-interval",
         "dhcp-listen-on-interfaces"
     );
 
-    remOption("dhcp-option", "dhcp-server-config");
+    //se eliminan opciones de la opciones avanzadas
+    remOption("dhcp-option", "dhcp-server-config"); 
     remove($dhcpTable);
+
+    //se elimina el timer de alquiler asociado al servidor DHCP
+    clearInterval(serverLeaseTimers[networkObjectId]); 
+    delete serverLeaseTimers[networkObjectId];
 
     terminalMessage("DHCP Server desinstalado correctamente.", networkObjectId);
 
