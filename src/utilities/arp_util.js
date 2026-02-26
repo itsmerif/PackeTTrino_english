c6dfc0d61054b82a91f963381bb278bd55f2a0a5
itsmeRiF <arif.ali.khan12@gmail.com>
2026-02-26 14:18:51 +0530

    Update arp_util.js

--- src/utilities/arp_util.js
+++ src/utilities/arp_util.js
@@ -8,10 +8,10 @@ function command_arp(networkObjectId, args) {
 
     if (args[1] === "-f" || args[1] === "--flush") {
         clearARPTable(networkObjectId)
-        terminalMessage("La tabla ARP ha sido limpiada correctamente.", networkObjectId);
+        terminalMessage("The ARP table was successfully cleaned.", networkObjectId);
         return;
     }
 
-    terminalMessage("Error: Sintaxis: arp &lt;-n&gt; ", networkObjectId);
+    terminalMessage("Error: Syntax: arp &lt;-n&gt; ", networkObjectId);
 
 }
