2a4e9831b831b61a72e0b58957b106cf8eb7acea
itsmeRiF <arif.ali.khan12@gmail.com>
2026-02-26 14:28:39 +0530

    Update dns_util.js

--- src/utilities/dns_util.js
+++ src/utilities/dns_util.js
@@ -5,7 +5,7 @@ function command_dns(networkObjectId, args) {
     const isNamedOn = $serverObject.getAttribute("named") === "true";
 
     if (!isNamedOn) {
-        terminalMessage("Error: Utilidad no disponible en este equipo.", networkObjectId);
+        terminalMessage("Error: Utility not available on this computer.", networkObjectId);
         return;
     }
     
@@ -18,13 +18,13 @@ function command_dns(networkObjectId, args) {
 
         $dnsTable.innerHTML = `
             <tr>
-                <th>Dominio</th>
-                <th>Tipo</th>
-                <th>Valor</th>
+                <th>Domain</th>
+                <th>Type</th>
+                <th>Value</th>
             </tr>
         `;
 
-        terminalMessage("La tabla de registros DNS ha sido limpiada correctamente.", networkObjectId);
+        terminalMessage("The DNS records table has been successfully cleaned.", networkObjectId);
         return;
     }
 
@@ -35,7 +35,7 @@ function command_dns(networkObjectId, args) {
             const value = args[3];
             let record = new dnsRecord(domain, "A", value);
             addDnsEntry(networkObjectId, record);
-            terminalMessage("Se ha añadido correctamente el registro DNS.", networkObjectId);
+            terminalMessage("The DNS record was successfully added.", networkObjectId);
             return;
         }
 
@@ -71,7 +71,7 @@ function command_dns(networkObjectId, args) {
                     record.cacheTTL = cacheTTL;
 
                     addDnsEntry(networkObjectId, record);
-                    terminalMessage(`Se ha añadido correctamente el registro ${recordType.toUpperCase()}.`, networkObjectId);
+                    terminalMessage(`The record has been successfully added ${recordType.toUpperCase()}.`, networkObjectId);
 
                 } catch (error) {
 
@@ -81,8 +81,8 @@ function command_dns(networkObjectId, args) {
 
             }else {
 
-                terminalMessage("Error: Tipo de Registro Desconocido.", networkObjectId);
-                terminalMessage("Error: Sintaxis: dns add -t &lt;tipo&gt; &lt;dominio&gt; &lt;valor&gt;", networkObjectId);
+                terminalMessage("Error: Unknown Record Type.", networkObjectId);
+                terminalMessage("Error: Syntax: dns add -t &lt;tipo&gt; &lt;dominio&gt; &lt;valor&gt;", networkObjectId);
 
             }
             
@@ -95,10 +95,10 @@ function command_dns(networkObjectId, args) {
         let recordType= args[2];
         let domain = args[3];
         delDnsEntry(networkObjectId, recordType, domain);
-        terminalMessage("Se ha eliminado correctamente el registro DNS.", networkObjectId);
+        terminalMessage("The DNS record was successfully deleted.", networkObjectId);
         return;
     }
        
-    terminalMessage("Error: Sintaxis: dns &lt;add|del&gt; [-t &lt;type&gt;] &lt;domain|cname&gt; [ip|domain]", networkObjectId);
+    terminalMessage("Error: Syntax: dns &lt;add|del&gt; [-t &lt;type&gt;] &lt;domain|cname&gt; [ip|domain]", networkObjectId);
 
 }
